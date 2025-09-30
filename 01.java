93a7ecf8d7094c4f2bb8fe971fcda9db19b0a54b
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-09-30 18:49:33 -0300

    Create 01.java

---
+++ 01.java
@@ -0,0 +1,11 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        String nome = "Carlos";
+        int idade = 35;
+        double altura = 1.80;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + "m");
+    }
+}
