69415c79e037f12cb53ec8739c3303993eed617d
Rodolfo Clepf <68128390+rclepf@users.noreply.github.com>
2025-09-30 18:50:40 -0300

    Create 05.java

---
+++ 05.java
@@ -0,0 +1,12 @@
+public class Exercicio05 {
+    public static void main(String[] args) {
+        String primeiroNome = "Ana";
+        String sobrenome = "Santos";
+        int idade = 28;
+        char primeiraLetraNome = 'A';
+
+        System.out.println("Meu nome é " + primeiroNome + " " + sobrenome +
+                           ", tenho " + idade + " anos. Meu nome começa com a letra " +
+                           primeiraLetraNome + ".");
+    }
+}
